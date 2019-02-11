27a07c3bf0a4a77e649ea3b7b8a126ad0782c067
KasperBK <45703936+KasperBK@users.noreply.github.com>
2019-02-11 23:28:43 +0800

    Added new functions

--- app/Http/Controllers/InventoryController.php
+++ app/Http/Controllers/InventoryController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 use App\InventoryPost;
 use DB;
 use App\Quotation;
-
+use Session;
 
 class InventoryController extends Controller
 {
@@ -28,7 +28,7 @@ class InventoryController extends Controller
      * @return \Illuminate\Http\Response
      */
 
-
+    /* Create Item Function */
     function createItem(Request $req)
     {
         $icode=$req->input('ic');
@@ -40,17 +40,77 @@ class InventoryController extends Controller
         $iquan=$req->input('iq');
         $iaquan=$req->input('iaq');
         $exist = DB::table('inventory')->where('Item_Code', $icode)->first();
-            if ($exist === null) {
+        if ($exist === null) {
    // user doesn't exist
 
-                $data=array('Item_Category'=>$icateg,'Item_Brand'=>$ibr,'Item_Code'=>$icode,'Item_Description'=>$idesc,'Item_Type'=>0,'Alarm_Quantity'=>$iaquan,'Item_Quantity'=>$iquan,'Item_Unit'=>$iunit,'Item_Price'=>$ipri);
-                DB::table('inventory')->insert($data);
+            $data=array('Item_Category'=>$icateg,'Item_Brand'=>$ibr,'Item_Code'=>$icode,'Item_Description'=>$idesc,'Item_Type'=>0,'Alarm_Quantity'=>$iaquan,'Item_Quantity'=>$iquan,'Item_Unit'=>$iunit,'Item_Price'=>$ipri);
+            DB::table('inventory')->insert($data);
 
-                return redirect()->back()->with('alert', $icode.' Added to Inventory!');
-            }
-            else{
+            return redirect()->back()->with('alert', $icode.' Added to Inventory!');
+        }
+        else{
 
-                return redirect()->back()->with('alert', $icode.' Already Exists!');   
-            }
+            return redirect()->back()->with('alert', $icode.' Already Exists!');   
         }
     }
+
+    /*Populate Update Item Form Function*/
+    function popItemForm(Request $req)
+    {
+
+        $itemID=$req->input('itemID');
+        $iteminfo = DB::table('inventory')->where('Item_ID',$itemID)->get(); 
+        echo json_encode($iteminfo);
+    } 
+
+
+
+    //update Item
+    function updateItem(Request $req)
+    {
+        $iid=$req->input('uid');
+        $icode=$req->input('ic');
+        $idesc=$req->input('id');
+        $ibr=$req->input('ib');
+        $icateg=$req->input('icat');
+        $ipri=$req->input('ip');
+        $iunit=$req->input('iuom');
+        $iquan=$req->input('iq');
+        $iaquan=$req->input('iaq');
+
+        $data=array('Item_Category'=>$icateg,'Item_Brand'=>$ibr,'Item_Code'=>$icode,'Item_Description'=>$idesc,'Item_Type'=>0,'Alarm_Quantity'=>$iaquan,'Item_Quantity'=>$iquan,'Item_Unit'=>$iunit,'Item_Price'=>$ipri);
+        DB::table('inventory')->where('Item_ID',$iid)->update($data);
+
+        return redirect()->back()->with('alert', 'Updated Item '.$icode.' info!');
+        
+    }
+
+
+    //archive Item
+    function archiveItem(Request $req)
+    {
+        $iid=$req->input('aid');
+        $iic=$req->input('aic');
+
+        $data=array('archive'=>'1');
+        DB::table('inventory')->where('Item_ID',$iid)->update($data);
+
+        return redirect()->back()->with('alert', 'Archived Item '.$iic);
+        
+    }
+
+     //archive Item
+    function unarchiveItem(Request $req)
+    {
+        $iid=$req->input('aid');
+        $iic=$req->input('aic');
+
+        $data=array('archive'=>'0');
+        DB::table('inventory')->where('Item_ID',$iid)->update($data);
+
+        return redirect()->back()->with('alert', 'Restored Item '.$iic);
+        
+    }
+
+
+}
