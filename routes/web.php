14cd93c87145e33e25cbd91c132aa9d96a2c7682
KasperBK <45703936+KasperBK@users.noreply.github.com>
2019-02-26 14:39:48 +0800

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,4 @@
 <?php
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -10,58 +9,87 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-
+Route::resource('Billing', 'BillingController');
 Route::get('/',function(){
 	return view('home');
 });
 Route::get('/customers',function(){
-	return view('customers');
+	return view('sales_templates.customers');
 });
 Route::get('/login',function(){
 	return view('login');
 });
 Route::get('/store',function(){
-	return view('store');
+	return view('sales_templates.store');
 });
 Route::get('/items',function(){
-	return view('items');
+	return view('sales_templates.items');
 });
 
 
-Route::get('/inventoryNotifs',function(){
-	return view('inventory/index_inventory');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Route::get('/selectInventory',function(){
+	$inventories = \DB::table('inventory')->where('archive',0)->where('Item_Type',0)->orderBy('item_code','ASC')->get();
+	return view('inventory.index_inventory')->with('inventories',$inventories);
 });
-Route::get('/inventory',function(){
-	return view('inventory/inventory');
+Route::get('/inventoryMain',function(){
+	$inventories = \DB::table('inventory')->where('archive',0)->orderBy('item_code','ASC')->get();
+	return view('inventory.inventory')->with('inventories',$inventories);
 });
+
 Route::get('/archiveInventory',function(){
-	return view('inventory/archive_inventory');
+	return view('inventory.archive_inventory');
 });
 
 Route::get('/category&brands',function(){
-	return view('inventory/category_brands');
+	return view('inventory.category_brands');
 });
 Route::get('/archiveCategory&Brands',function(){
-	return view('inventory/archive_category&brands');
+	return view('inventory.archive_category&brands');
+});
+
+Route::get('/inventoryReports',function(){
+	return view('inventory.inventoryReports');
 });
 
 
-Route::resource('Billing', 'BillingController');
-Route::resource('Inventory', 'InventoryController');
 
+Route::resource('Inventory', 'InventoryController');
 Route::post('/createItem','InventoryController@createItem');
+Route::post('/createPackage','InventoryController@createPackage');
 Route::post('/createBrand','InventoryController@createBrand');
 Route::post('/createCategory','InventoryController@createCategory');
 Route::post('/updateItem','InventoryController@updateItem');
+Route::post('/updatePckgInfo','InventoryController@updatePckgInfo');
+Route::post('/updatePckgList','InventoryController@updatePckgList');
 Route::post('/updateBrand','InventoryController@updateBrand');
 Route::post('/updateCategory','InventoryController@updateCategory');
 Route::post('/archiveItem','InventoryController@archiveItem')->name("archiveItem");
+Route::post('/deleteItem','InventoryController@deleteItem')->name("deleteItem");
 Route::post('/archiveBrand','InventoryController@archiveBrand')->name("archiveBrand");
 Route::post('/archiveCategory','InventoryController@archiveCategory')->name("archiveCategory");
 Route::post('/unarchiveItem','InventoryController@unarchiveItem')->name("unarchiveItem");
 Route::post('/unarchiveBrand','InventoryController@unarchiveBrand')->name("unarchiveBrand");
 Route::post('/unarchiveCategory','InventoryController@unarchiveCategory')->name("unarchiveCategory");
+Route::post('/deleteBrand','InventoryController@deleteBrand')->name("deleteBrand");
+Route::post('/deleteCategory','InventoryController@deleteCategory')->name("deleteCategory");
 Route::post('/popItemForm','InventoryController@popItemForm')->name("popItemForm");
+Route::post('/popPckgList','InventoryController@popPckgList')->name("popPckgList");
 Route::post('/popBrandForm','InventoryController@popBrandForm')->name("popBrandForm");
 Route::post('/popCategoryForm','InventoryController@popCategoryForm')->name("popCategoryForm");
+Route::post('/getInvAlerts','InventoryController@getInvAlerts')->name("getInvAlerts");
+Route::post('/getPic','InventoryController@getPic')->name("getPic");
+?>
