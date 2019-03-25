34990583d8d6afb9684206eda404f0ce379b53b7
Sai-PC\Sai <michelleannesayson24@gmail.com>
2019-03-26 02:34:54 +0800

    Item View - inventory

--- resources/views/Inventory/modal-invt/viewItemModal.blade.php
+++ resources/views/Inventory/modal-invt/viewItemModal.blade.php
@@ -8,33 +8,54 @@
 				<div class="modal-body">
 					<div class="row">
 						<div class="offset-md-1 col-sm-4">
-							<img src="{{asset('inventory/none.png')}}" style="height: 200px; width: 200px; border:1px solid black;">
+							{{-- <img src="{{asset('inventory/none.png')}}" style="height: 200px; width: 200px; border:1px solid black;"> --}}
+							<div id="itemInformation" class="carousel slide" data-interval="false">
+								<ul class="carousel-indicators">
+									<li data-target="#itemInformation" data-slide-to="0" class="active"></li>
+									<li data-target="#itemInformation" data-slide-to="1"></li>
+									<li data-target="#itemInformation" data-slide-to="2"></li>
+								</ul>
+								<div class="carousel-inner">
+									<div class="carousel-item active">
+										<img src="{{asset('inventory/none.png')}}" alt="Category" style="height: 200px; width: 200px; border:1px solid black;"> 
+									</div>
+									<div class="carousel-item">
+										<img src="{{asset('inventory/none.png')}}" alt="Brand" style="height: 200px; width: 200px; border:1px solid black;">
+									</div>
+								</div>
+								<a class="carousel-control-prev" href="#itemInformation" data-slide="prev">
+									<span class="carousel-control-prev-icon"></span>
+								</a>
+								<a class="carousel-control-next" href="#itemInformation" data-slide="next">
+									<span class="carousel-control-next-icon"></span>
+								</a>
+							</div>
 						</div>
 						<div class="col-sm-5">
 							<div class="card">
 								<div class="card-body">
 									<b><span class="info-labels" id="itemcode">Item Code</span><br>
-									<span class="info-labels" id="brand">Brand</span><br>
-									<span class="info-labels" id="category">Category</span><br></b>
-									<span class="info-labels" id="quantity">Quantity</span>
+										<span class="info-labels" id="brand">Brand</span><br>
+										<span class="info-labels" id="category">Category</span><br></b>
+										<span class="info-labels" id="quantity">Quantity</span>
+									</div>
 								</div>
 							</div>
 						</div>
-					</div>
-					<hr>
-					<div class="row">
-						<div class="col-sm-12">
-							<h4 class="info-labels">Description</h4>
-							<span class="info-labels" id="description">Body</span>
+						<hr>
+						<div class="row">
+							<div class="col-sm-12">
+								<h4 class="info-labels">Description</h4>
+								<span class="info-labels" id="description">Body</span>
+							</div>
 						</div>
-					</div>
-					<hr>
-					<div class="row">
-						<div class="col-sm-12">
-							<h4 class="info-labels">Items Included</h4>
+						<hr>
+						<div class="row">
+							<div class="col-sm-12">
+								<h4 class="info-labels">Items Included</h4>
+							</div>
 						</div>
 					</div>
 				</div>
 			</div>
-		</div>
 </div> {{-- end VIEW item --}}
